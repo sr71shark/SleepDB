0baf1bada1c2ab1234f536374d8273200cdfeae9
sr71shark <markwhiteside@mac.com>
2019-04-30 16:52:39 -0600

    Fixed broken calculators

--- SleepCalc.py
+++ SleepCalc.py
@@ -1,5 +1,5 @@
 import sqlite3
-from datetime import datetime
+import datetime
 
 dbconn = sqlite3.connect("/Users/mwhites/programming/python/SleepLog/sleep.db")
 #dbconn = sqlite3.connect('C:\Users\Andrew\Documents\Programming\Python\SleepDB\sleep.db')
@@ -12,36 +12,29 @@ info = c.fetchall()
 RecentDate = info[0][0]
 print(RecentDate)
 
-RecentWakeTime = datetime.strptime("{0} {1}".format(info[0][0],info[0][1]), "%m/%d/%Y %I:%M %p")
-RecentBedOut = datetime.strptime("{0} {1}".format(info[0][0],info[0][2]), "%m/%d/%Y %I:%M %p")
+RecentWakeTime = datetime.datetime.strptime("{0} {1}".format(info[0][0],info[0][1]), "%m/%d/%Y %I:%M %p")
+RecentBedOut = datetime.datetime.strptime("{0} {1}".format(info[0][0],info[0][2]), "%m/%d/%Y %I:%M %p")
 RecentTotalEA = info[0][5]
 
-# print(RecentWakeTime)
-# print(RecentBedOut)
-# print(RecentTotalEA)
-
-PriorBedTime = datetime.strptime("{0} {1}".format(info[1][0],info[1][3]), "%m/%d/%Y %I:%M %p")
-PriorApproximateSleepTime = datetime.strptime("{0} {1}".format(info[1][0],info[1][4]), "%m/%d/%Y %I:%M %p")
-
-# print(PriorBedTime)
-# print(PriorApproximateSleepTime)
+PriorBedTime = datetime.datetime.strptime("{0} {1}".format(info[1][0],info[1][3]), "%m/%d/%Y %I:%M %p")
+PriorApproximateSleepTime = datetime.datetime.strptime("{0} {1}".format(info[1][0],info[1][4]), "%m/%d/%Y %I:%M %p")
 
 def TimeInBedCalc(bt, bo):
     TimeInBed = bo - bt
     TimeInBed = str(TimeInBed)
-    #print(TimeInBed[0:-3])
+    print(TimeInBed[0:-3])
     return TimeInBed[0:-3]
 
 
 def TimeAsleepCalc(ast, wt, ea):
     TimeAsleep = (wt - ast) - datetime.timedelta(minutes = ea)
     TimeAsleep = str(TimeAsleep)
-    #print(TimeAsleep[0:-3])
+    print(TimeAsleep[0:-3])
     return TimeAsleep[0:-3]
 
 
-# TimeInBedCalc(PriorBedTime, RecentBedOut)
-# TimeAsleepCalc(PriorApproximateSleepTime, RecentWakeTime, RecentTotalEA)
+TimeInBedCalc(PriorBedTime, RecentBedOut)
+TimeAsleepCalc(PriorApproximateSleepTime, RecentWakeTime, RecentTotalEA)
 
 #x = TimeAsleepCalc(PriorApproximateSleepTime, RecentWakeTime, RecentTotalEA)
 #print("x=: " + x)
