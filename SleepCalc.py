9a9514f46ebd6eb5d5107bba9de9f4066cba821d
BuildTools <unconfigured@null.spigotmc.org>
2019-04-30 18:05:10 -0400

    Edited to show mark how it saves.

--- SleepCalc.py
+++ SleepCalc.py
@@ -1,7 +1,8 @@
 import sqlite3
 import datetime
 
-dbconn = sqlite3.connect("/Users/mwhites/programming/python/SleepLog/sleep.db")
+#dbconn = sqlite3.connect("/Users/mwhites/programming/python/SleepLog/sleep.db")
+dbconn = sqlite3.connect('C:\Users\Andrew\Documents\Programming\Python\SleepDB\sleep.db')
 c = dbconn.cursor()
 # dateOne = '04/26/2019'
 # dateTwo = '04/24/2019'
@@ -12,24 +13,26 @@ info = c.fetchall()
 
 dbconn.close()
 
-print(info[0])
-print(info[1])
+# print(info[0])
+# print(info[1])
 
-RecentDate = datetime.datetime.strptime("{0} {1}".format(info[0][0]))
-print(RecentDate)
-RecentWakeTime = datetime.datetime.strptime("{0} {1}".format(info[0][0],info[0][1]), "%m/%d/%Y %I:%M %p")
+print("Here is the Info: " + info[0][0])
+
+# RecentDate = datetime.datetime.strptime("{0} {1}".format(info[0][0]))
+# print(RecentDate)
+RecentWakeTime = datetime.datetime.strptime("{0} {1}", "%m/%d/%Y %I:%M %p")#.format(info[0][0],info[0][1]))
 RecentBedOut = datetime.datetime.strptime("{0} {1}".format(info[0][0],info[0][2]), "%m/%d/%Y %I:%M %p")
 RecentTotalEA = info[0][5]
 
-print(RecentWakeTime)
-print(RecentBedOut)
-print(RecentTotalEA)
+# print(RecentWakeTime)
+# print(RecentBedOut)
+# print(RecentTotalEA)
 
 PriorBedTime = datetime.datetime.strptime("{0} {1}".format(info[1][0],info[1][3]), "%m/%d/%Y %I:%M %p")
 PriorApproximateSleepTime = datetime.datetime.strptime("{0} {1}".format(info[1][0],info[1][4]), "%m/%d/%Y %I:%M %p")
 
-print(PriorBedTime)
-print(PriorApproximateSleepTime)
+# print(PriorBedTime)
+# print(PriorApproximateSleepTime)
 
 def TimeInBedCalc(bt, bo):
     TimeInBed = bo - bt
