646e10067741f549af624892079fe38b8b2f5156
sr71shark <markwhiteside@mac.com>
2019-04-30 16:43:24 -0600

    Made things more compact.

--- SleepCalc.py
+++ SleepCalc.py
@@ -1,33 +1,17 @@
 import sqlite3
 from datetime import datetime
 
-#dbconn = sqlite3.connect("/Users/mwhites/programming/python/SleepLog/sleep.db")
-dbconn = sqlite3.connect('C:\Users\Andrew\Documents\Programming\Python\SleepDB\sleep.db')
+dbconn = sqlite3.connect("/Users/mwhites/programming/python/SleepLog/sleep.db")
+#dbconn = sqlite3.connect('C:\Users\Andrew\Documents\Programming\Python\SleepDB\sleep.db')
 c = dbconn.cursor()
-# dateOne = '04/26/2019'
-# dateTwo = '04/24/2019'
 
 c.execute('''SELECT Date, WakeTime, BedOut, BedTime, ApproximateSleepTime, 
-                    TotalEA FROM SleepCycle ORDER BY date DESC LIMIT 2''')
+             TotalEA FROM SleepCycle ORDER BY date DESC LIMIT 2''')
 info = c.fetchall()
 
+RecentDate = info[0][0]
+print(RecentDate)
 
-
-# print(info[0])
-# print(info[1])
-
-print("Here is the Info: " + info[0][0])
-# newdateone = str(info[0][0]) + " 00:00 PM"
-# newdatetwo = str(info[1][0]) + " 00:00 PM"
-# print newdateone
-# rd = datetime.strptime(info[0][0], "%m/%d/%Y %I:%M %p")
-
-# TODO THIS IS A CODE FOR MARK TO REEEEEAAAAD
-
-
-
-# RecentDate = datetime.strptime(newdateone, '%m/%d/%Y')
-# print(RecentDate)
 RecentWakeTime = datetime.strptime("{0} {1}".format(info[0][0],info[0][1]), "%m/%d/%Y %I:%M %p")
 RecentBedOut = datetime.strptime("{0} {1}".format(info[0][0],info[0][2]), "%m/%d/%Y %I:%M %p")
 RecentTotalEA = info[0][5]
@@ -62,7 +46,7 @@ def TimeAsleepCalc(ast, wt, ea):
 #x = TimeAsleepCalc(PriorApproximateSleepTime, RecentWakeTime, RecentTotalEA)
 #print("x=: " + x)
 
-c.execute("SELECT Date From SleepCycle WHERE date={}".format(info[0][0]))
+c.execute("SELECT * FROM SleepCycle WHERE date={0}".format(RecentDate))
 newvar = c.fetchall()
 print(newvar)
 dbconn.close()
