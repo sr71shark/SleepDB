9b60a5afae1b0ed632b551087c6b960fdd578931
RobAShear <andyshear12@gmail.com>
2019-05-01 19:30:56 -0400

    Updated db with correct variables and committed.

--- SleepCalc.py
+++ SleepCalc.py
@@ -9,26 +9,24 @@ c.execute('''SELECT Date, WakeTime, BedOut, BedTime, ApproximateSleepTime,
 info = c.fetchall()
 
 RecentDate = info[0][0]
-print(RecentDate)
 
 RecentWakeTime = datetime.datetime.strptime("{0} {1}".format(info[0][0],info[0][1]), "%m/%d/%Y %I:%M %p")
 RecentBedOut = datetime.datetime.strptime("{0} {1}".format(info[0][0],info[0][2]), "%m/%d/%Y %I:%M %p")
 RecentTotalEA = info[0][5]
 
 PriorBedTime = datetime.datetime.strptime("{0} {1}".format(info[1][0],info[1][3]), "%m/%d/%Y %I:%M %p")
-PriorApproximateSleepTime = datetime.datetime.strptime("{0} {1}".format(info[1][0],info[1][4]), "%m/%d/%Y %I:%M %p")
+PriorApproximateSleepTime = datetime.datetime.strptime("{0} {1}".format(info[1][0], info[1][4]), "%m/%d/%Y %I:%M %p")
+
 
 def TimeInBedCalc(bt, bo):
     TimeInBed = bo - bt
     TimeInBed = str(TimeInBed)
-    print("Time In Bed: {}".format(TimeInBed[0:-3]))
     return TimeInBed[0:-3]
 
 
 def TimeAsleepCalc(ast, wt, ea):
-    TimeAsleep = (wt - ast) - datetime.timedelta(minutes = ea)
+    TimeAsleep = (wt - ast) - datetime.timedelta(minutes=ea)
     TimeAsleep = str(TimeAsleep)
-    print("Time Asleep: {}".format(TimeAsleep[0:-3]))
     return TimeAsleep[0:-3]
 
 
@@ -37,8 +35,9 @@ TimeAsleepCalc(PriorApproximateSleepTime, RecentWakeTime, RecentTotalEA)
 
 c.execute("SELECT * FROM SleepCycle WHERE date='{0}'".format(RecentDate))
 
-
-# TODO Do something with this return variable
-newvar = c.fetchall()
-print(newvar[0])
+# TODO Assuming update the current database with current date.
+c.execute('UPDATE SleepCycle SET TimeInBed="{0}", TimeAsleep="{1}" WHERE date="{2}"'.format(
+    TimeInBedCalc(PriorBedTime, RecentBedOut), TimeAsleepCalc(PriorApproximateSleepTime, RecentWakeTime, RecentTotalEA),
+    RecentDate))
+dbconn.commit()
 dbconn.close()
