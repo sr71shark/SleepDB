3b443f16cb9237f4883959e3e35df8ab8a3ed36e
RobAShear <andyshear12@gmail.com>
2019-05-01 20:18:53 -0400

    Multiple Files Edited. Added Exception control. Sent sleep.db with errors on purpose.

--- CreateLog.py
+++ CreateLog.py
@@ -2,9 +2,7 @@ import sqlite3
 
 
 dbconn = sqlite3.connect("sleep.db")
-
 c = dbconn.cursor()
-print("pass")
 
 c.execute("""CREATE TABLE SleepCycle (Date datetime_int, ApproximateSleepTime timestamp, EveningAwake int, TotalEA int, 
 WakeTime datetime_int, BedOut datetime_int, Comments text, SleepQual int, SubsAffect text, naps int, Exercise text, 
--- SleepCalc.py
+++ SleepCalc.py
@@ -1,22 +1,6 @@
 import sqlite3
 import datetime
-
-dbconn = sqlite3.connect('sleep.db')
-c = dbconn.cursor()
-
-c.execute('''SELECT Date, WakeTime, BedOut, BedTime, ApproximateSleepTime, 
-             TotalEA FROM SleepCycle ORDER BY date DESC LIMIT 2''')
-info = c.fetchall()
-
-RecentDate = info[0][0]
-
-RecentWakeTime = datetime.datetime.strptime("{0} {1}".format(info[0][0],info[0][1]), "%m/%d/%Y %I:%M %p")
-RecentBedOut = datetime.datetime.strptime("{0} {1}".format(info[0][0],info[0][2]), "%m/%d/%Y %I:%M %p")
-RecentTotalEA = info[0][5]
-
-PriorBedTime = datetime.datetime.strptime("{0} {1}".format(info[1][0],info[1][3]), "%m/%d/%Y %I:%M %p")
-PriorApproximateSleepTime = datetime.datetime.strptime("{0} {1}".format(info[1][0], info[1][4]), "%m/%d/%Y %I:%M %p")
-
+import logging
 
 def TimeInBedCalc(bt, bo):
     TimeInBed = bo - bt
@@ -30,14 +14,35 @@ def TimeAsleepCalc(ast, wt, ea):
     return TimeAsleep[0:-3]
 
 
-TimeInBedCalc(PriorBedTime, RecentBedOut)
-TimeAsleepCalc(PriorApproximateSleepTime, RecentWakeTime, RecentTotalEA)
+dbconn = sqlite3.connect('sleep.db')
+c = dbconn.cursor()
 
-c.execute("SELECT * FROM SleepCycle WHERE date='{0}'".format(RecentDate))
+c.execute('''SELECT Date, WakeTime, BedOut, BedTime, ApproximateSleepTime, 
+             TotalEA FROM SleepCycle ORDER BY date DESC LIMIT 2''')
+info = c.fetchall()
+try:
+    RecentDate = info[0][0]
 
-# TODO Assuming update the current database with current date.
-c.execute('UPDATE SleepCycle SET TimeInBed="{0}", TimeAsleep="{1}" WHERE date="{2}"'.format(
-    TimeInBedCalc(PriorBedTime, RecentBedOut), TimeAsleepCalc(PriorApproximateSleepTime, RecentWakeTime, RecentTotalEA),
-    RecentDate))
-dbconn.commit()
+    RecentWakeTime = datetime.datetime.strptime("{0} {1}".format(info[0][0], info[0][1]), "%m/%d/%Y %I:%M %p")
+    RecentBedOut = datetime.datetime.strptime("{0} {1}".format(info[0][0], info[0][2]), "%m/%d/%Y %I:%M %p")
+    RecentTotalEA = info[0][5]
+
+    PriorBedTime = datetime.datetime.strptime("{0} {1}".format(info[1][0], info[1][3]), "%m/%d/%Y %I:%M %p")
+    PriorApproximateSleepTime = datetime.datetime.strptime("{0} {1}".format(info[1][0], info[1][4]), "%m/%d/%Y %I:%M %p")
+
+    tibc = TimeInBedCalc(PriorBedTime, RecentBedOut)
+    tac = TimeAsleepCalc(PriorApproximateSleepTime, RecentWakeTime, RecentTotalEA)
+
+    c.execute("SELECT * FROM SleepCycle WHERE date='{0}'".format(RecentDate))
+
+    # TODO Assuming update the current database with current date.
+    c.execute('UPDATE SleepCycle SET TimeInBed="{0}", TimeAsleep="{1}" WHERE date="{2}"'.format(tibc, tac, RecentDate))
+    dbconn.commit()
+except ValueError as e:
+    print("Error found at:\t \033[91m{}\033[0m".format(e))
+    print("Data missing from the database to do the calculations.")
+except Exception as e:
+    logging.basicConfig(level=logging.DEBUG, filename='errors.log')
+    logging.exception("Exception Occurred:")
+
 dbconn.close()
